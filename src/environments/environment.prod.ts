45900a9ffde2c256087b12c369c2200b83e520d2
Krzysiek S <krzszk96@gmail.com>
2022-07-11 14:12:50 +0200

    test env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  rawgApiKey: '01f4e396175646ff84d319d91453def7',
+  RapidApiKey: '80fdc48ae6msh69472610a1d0830p12fb95jsn1f786b33516c'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  rawgApiKey: '01f4e396175646ff84d319d91453def7',
+  RapidApiKey: '80fdc48ae6msh69472610a1d0830p12fb95jsn1f786b33516c'
 };
 
 /*
